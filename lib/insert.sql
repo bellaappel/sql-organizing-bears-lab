d84493ac56f78885ffb91afb25d4b9fa0fb5db2f
bellaappel <bellaappel0@gmail.com>
2020-10-26 20:33:39 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, alive) VALUES ( "Mr.Chocolate", 5, "M", "Brown", 1);
+INSERT INTO bears (name, age, gender, color, alive) VALUES ( "Rowdy", 3, "F", "Tan", 1);  
+INSERT INTO bears (name, age, gender, color, alive) VALUES ( "Tabita", 2, "F", "Brown", 1); 
+INSERT INTO bears (name, age, gender, color, alive) VALUES ( "Sergeant Brown", 3, "M", "Brown", 1);
+INSERT INTO bears (name, age, gender, color, alive) VALUES ( "Melissa", 1, "F", "Red", 1); 
+INSERT INTO bears (name, age, gender, color, alive) VALUES ( "Grinch", 5, "M", "White", 1); 
+INSERT INTO bears (name, age, gender, color, alive) VALUES ( "Wendy", 5, "F", "Brown", 1); 
+INSERT INTO bears (name, age, gender, color, alive) VALUES (NULL, 5, "M", "Brown", 1); 
